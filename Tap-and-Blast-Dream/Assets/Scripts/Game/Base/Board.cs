750361128863a423c06043ef8c6e79e5fdaff832
Zeynep Sumer <sumerszeynep@gmail.com>
2024-04-02 17:50:38 +0300

    handle blast fix

--- Tap-and-Blast-Dream/Assets/Scripts/Game/Base/Board.cs
+++ Tap-and-Blast-Dream/Assets/Scripts/Game/Base/Board.cs
@@ -8,8 +8,8 @@ using UnityEngine.UI;
 using DG.Tweening;
 public class Board : MonoBehaviour
 {
-    public int Width {get; private set;}
-    public int Height {get; private set;}
+    public int Width { get; private set; }
+    public int Height { get; private set; }
 
     [SerializeField] private GameObject _container;
     [SerializeField] private RectTransform _grid;
@@ -24,31 +24,35 @@ public class Board : MonoBehaviour
     private Dictionary<Vector2Int, Vector2> _gridToWorldPos;
 
 
-    private void Awake() {
-      _factory = new BoardEntityFactory(transform);
+    private void Awake()
+    {
+        _factory = new BoardEntityFactory(transform);
     }
 
-   public void Set(int width, int height, string[] grid) {
-    Width = width;
-    Height = height;
-    Items = new BoardEntity[Width, Height];
-    _gridToWorldPos = new Dictionary<Vector2Int, Vector2>();
-    SetBoard(grid);
-   }
+    public void Set(int width, int height, string[] grid)
+    {
+        Width = width;
+        Height = height;
+        Items = new BoardEntity[Width, Height];
+        _gridToWorldPos = new Dictionary<Vector2Int, Vector2>();
+        SetBoard(grid);
+    }
 
-    private void SetBoard(string[] grid) {
+    private void SetBoard(string[] grid)
+    {
 
         BoardEntity[] entityArr = GetGridItems(grid);
-        
-      if (_container == null)
+
+        if (_container == null)
         {
             Debug.LogWarning("Container of board is empty")
-;           return;
+; return;
         }
 
-      if(entityArr == null || entityArr.Length <= 0) {
-        return;
-      }
+        if (entityArr == null || entityArr.Length <= 0)
+        {
+            return;
+        }
         SetGridLayout(_container);
 
         int count = 0;
@@ -60,31 +64,33 @@ public class Board : MonoBehaviour
                 if (count < entityArr.Length)
                 {
                     BoardEntity entity = entityArr[count];
-                    entity.name =  "Entity - " + i + " , " + j;
+                    entity.name = "Entity - " + i + " , " + j;
                     entity.transform.SetParent(_container.transform, true);
                     entity.transform.localScale = Vector2.one;
                     StartCoroutine(CoWaitForPosition(entity));
                     Items[i, j] = entity;
                     count++;
-                } else
-                {
-                  Debug.LogWarning("Error in SetBoard: count excedes grid length");
                 }
-               
+                else
+                {
+                    Debug.LogWarning("Error in SetBoard: count excedes grid length");
+                }
+
             }
         }
 
-        }
-
-
-private IEnumerator CoWaitForPosition(BoardEntity entity)
-{
-    if (entity == null) {
-      yield return null;
     }
-    yield return new WaitForEndOfFrame();
-    _gridToWorldPos.Add(GetPositionOfItem(entity), entity.transform.position);
-}
+
+
+    private IEnumerator CoWaitForPosition(BoardEntity entity)
+    {
+        if (entity == null)
+        {
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+        _gridToWorldPos.Add(GetPositionOfItem(entity), entity.transform.position);
+    }
 
     private void SetGridLayout(GameObject container)
     {
@@ -93,13 +99,14 @@ private IEnumerator CoWaitForPosition(BoardEntity entity)
             layout.constraint = GridLayoutGroup.Constraint.FixedRowCount;
             layout.constraintCount = Height;
 
-            var sampleCube =_factory.GetCube(1);
+            var sampleCube = _factory.GetCube(1);
             sampleCube.name = "sampleCube";
-            if (sampleCube != null && sampleCube.gameObject != null) {
+            if (sampleCube != null && sampleCube.gameObject != null)
+            {
                 if (sampleCube.TryGetComponent<RectTransform>(out var cubeRT))
                 {
                     float _itemWidth = cubeRT.rect.width;
-                    _cellSize = new Vector2(.75f, .75f)* _itemWidth;
+                    _cellSize = new Vector2(.75f, .75f) * _itemWidth;
                     layout.cellSize = _cellSize;
                     GenerateGrid(layout.cellSize.x);
                 }
@@ -118,177 +125,216 @@ private IEnumerator CoWaitForPosition(BoardEntity entity)
         }
     }
 
-    private BoardEntity[] GetGridItems(string[] grid) {
-      
-      BoardEntity[] arr = new BoardEntity[grid.Length];
+    private BoardEntity[] GetGridItems(string[] grid)
+    {
 
-      if (grid == null || grid.Length == 0) {
-        return arr;
-      }
-        for(int i = 0; i < grid.Length; i++) {
-           BoardEntity entity = StringToEntity(grid[i]);
-           if (entity != null) {
-            arr[i] = entity;
-           }
-        }
-        return arr;
-    }
+        BoardEntity[] arr = new BoardEntity[grid.Length];
 
-    private BoardEntity StringToEntity(string str) {
-
-      if (_factory == null) {
-        Debug.LogWarning("Factory is null");
-        return null;
-      }
-      if(str.Equals("r")) {
-        return _factory.GetCube((int) CubeTypes.Red);
-      }   
-      if(str.Equals("y")) {
-        return _factory.GetCube((int) CubeTypes.Yellow);
-      }      
-      if(str.Equals("b")) {
-        return _factory.GetCube((int) CubeTypes.Blue);
-      }      
-      if(str.Equals("g")) {
-        return _factory.GetCube((int) CubeTypes.Green);
-      }
-      if(str.Equals("rand")) {
-        return _factory.GetCube(Random.Range(1, 5));
-      }
-      if(str.Equals("tnt")) {
-        return _factory.GetTNT();
-      }
-      if (str.Equals("bo")) {
-        return _factory.GetBox();
-      }
-      if(str.Equals("s")){
-        return _factory.GetStone();
-      }
-      if(str.Equals("v")){
-        return _factory.GetVase();
-      }
-      else {
-        return _factory.GetCube(Random.Range(1, 5));
-      }
-        
-    }
-
-    public List<Obstacle> GetObstacles() {
-
-      List<Obstacle> obstacles = new List<Obstacle>();
-      foreach(BoardEntity entity in Items) {
-        if (entity.TryGetComponent(out Obstacle obstacle)) {
-          obstacles.Add(obstacle);
-        }
-      }
-      return obstacles;
-    }
-
-    public void Enable(bool isEnable) {
-     
-      _isEnabled = isEnable;
-      foreach(BoardEntity item in Items) {
-        if (item!= null && item.TryGetComponent(out Blastable blastable)) {
-          blastable.Enable(isEnable);
-        }
-      }
-       if (_container != null &&_container.TryGetComponent<GridLayoutGroup>(out var layout))
+        if (grid == null || grid.Length == 0)
         {
-          layout.enabled = isEnable;
+            return arr;
         }
-      
-    }
-
-    public bool IsEnabled() {
-      return _isEnabled;
-    }
-
-    public void RemoveItem(BoardEntity entity) {
-      if (entity == null) {
-        return;
-      }
-        Vector2Int pos = GetPositionOfItem(entity);
-        if(Items[pos.x, pos.y] == entity) {
-          Items[pos.x, pos.y] = null;
-          entity.Clear();
-          _factory.Return(entity);
-        
-      }
-      Events.GameEvents.OnBlastRemoved?.Invoke();
-    }
-
-    public void ReplaceItemsAfterBlast() {
-      MakeFalliblesFall();
-
-    }
-
-    private void MakeFalliblesFall() {
-      for(int j = 0; j < Height; j++) {
-        for(int i = 0; i < Width; i++) {
-          BoardEntity entity = Items[i,j];
-          if (entity == null) {
-            continue;
-          }
-          if (entity.TryGetComponent(out IFallible fallible)) {
-            Vector2Int bottomNeighborPos = new Vector2Int(i, j-1);
-            while(!IsIndexOutOfBounds(bottomNeighborPos) 
-            && Items[bottomNeighborPos.x, bottomNeighborPos.y] == null) {             
-                bottomNeighborPos = new Vector2Int(bottomNeighborPos.x, bottomNeighborPos.y - 1);
-              }
-              Vector2Int destinationPos = new Vector2Int(bottomNeighborPos.x, bottomNeighborPos.y + 1);
-              Items[i, j] = null;
-              Items[destinationPos.x, destinationPos.y] = entity;
-              entity.name = "Entity - " + destinationPos.x + " , " + destinationPos.y;
-              fallible.Fall(GetWorldPosition(destinationPos));
+        for (int i = 0; i < grid.Length; i++)
+        {
+            BoardEntity entity = StringToEntity(grid[i]);
+            if (entity != null)
+            {
+                arr[i] = entity;
             }
-      }
-      }
+        }
+        return arr;
     }
 
-    private void ReplaceFallibles() {
+    private BoardEntity StringToEntity(string str)
+    {
 
-      for(int i=0; i < Height ; i ++) {
-        for (int j=0; j < Width; j++) {
-          BoardEntity entity = Items[i, j];
+        if (_factory == null)
+        {
+            Debug.LogWarning("Factory is null");
+            return null;
+        }
+        if (str.Equals("r"))
+        {
+            return _factory.GetCube((int)CubeTypes.Red);
+        }
+        if (str.Equals("y"))
+        {
+            return _factory.GetCube((int)CubeTypes.Yellow);
+        }
+        if (str.Equals("b"))
+        {
+            return _factory.GetCube((int)CubeTypes.Blue);
+        }
+        if (str.Equals("g"))
+        {
+            return _factory.GetCube((int)CubeTypes.Green);
+        }
+        if (str.Equals("rand"))
+        {
+            return _factory.GetCube(Random.Range(1, 5));
+        }
+        if (str.Equals("tnt"))
+        {
+            return _factory.GetTNT();
+        }
+        if (str.Equals("bo"))
+        {
+            return _factory.GetBox();
+        }
+        if (str.Equals("s"))
+        {
+            return _factory.GetStone();
+        }
+        if (str.Equals("v"))
+        {
+            return _factory.GetVase();
+        }
+        else
+        {
+            return _factory.GetCube(Random.Range(1, 5));
+        }
+
+    }
+
+    public List<Obstacle> GetObstacles()
+    {
+
+        List<Obstacle> obstacles = new List<Obstacle>();
+        foreach (BoardEntity entity in Items)
+        {
+            if (entity.TryGetComponent(out Obstacle obstacle))
+            {
+                obstacles.Add(obstacle);
+            }
+        }
+        return obstacles;
+    }
+
+    public void Enable(bool isEnable)
+    {
+        _isEnabled = isEnable;
+        foreach (BoardEntity item in Items)
+        {
+            if (item != null && item.TryGetComponent(out Blastable blastable))
+            {
+                blastable.Enable(isEnable);
+            }
+        }
+    }
+
+    public bool IsEnabled()
+    {
+        return _isEnabled;
+    }
+
+    public void RemoveItem(BoardEntity entity)
+    {
+        if (entity == null)
+        {
+            return;
+        }
+        Vector2Int pos = GetPositionOfItem(entity);
+        if (Items[pos.x, pos.y] == entity)
+        {
+            Items[pos.x, pos.y] = null;
+            entity.Clear();
+            _factory.Return(entity);
 
         }
-      }
+    }
 
+    public void ReplaceItemsAfterBlast()
+    {
+      MakeFalliblesFall();
     }
 
 
-    private bool IsIndexOutOfBounds(Vector2Int pos) {
-      return (pos.x < 0 || pos.x >= Width || pos.y < 0 || pos.y >= Height);
-    }
-
-  
-    public Vector2 GetWorldPosition(Vector2Int boardPos) {
-      if (_gridToWorldPos.ContainsKeySafe(boardPos)) {
-        return _gridToWorldPos[boardPos];
-      } else {
-        return Vector2.negativeInfinity;
-      }
-    }
-
-    public Vector2Int GetPositionOfItem(BoardEntity entity) {
-    
-    if (Items == null || Items.Length == 0) {
-      return Vector2Int.down;
-    }
-    for(int i=0; i < Width; i++) {
-      for(int j=0; j < Height; j++) {
-        BoardEntity item = Items[i,j];
-        if (item!= null && item==entity) {
-          return new Vector2Int(i,j);
+    private void MakeFalliblesFall()
+    {
+        for (int j = 0; j < Height; j++)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                BoardEntity entity = Items[i, j];
+                if (entity == null)
+                {
+                    continue;
+                }
+                if (entity.TryGetComponent(out IFallible fallible))
+                {
+                    Vector2Int bottomNeighborPos = new Vector2Int(i, j - 1);
+                    while (!IsIndexOutOfBounds(bottomNeighborPos)
+                    && Items[bottomNeighborPos.x, bottomNeighborPos.y] == null)
+                    {
+                        bottomNeighborPos = new Vector2Int(bottomNeighborPos.x, bottomNeighborPos.y - 1);
+                    }
+                    Vector2Int destinationPos = new Vector2Int(bottomNeighborPos.x, bottomNeighborPos.y + 1);
+                    Items[i, j] = null;
+                    Items[destinationPos.x, destinationPos.y] = entity;
+                    entity.name = "Entity - " + destinationPos.x + " , " + destinationPos.y;
+                    fallible.Fall(GetWorldPosition(destinationPos));
+                }
+            }
         }
+    }
+
+    private void ReplaceFallibles()
+    {
+
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                BoardEntity entity = Items[i, j];
+
+            }
         }
-      }
-      return Vector2Int.down;
-    }
-    
 
     }
 
-    
+
+    private bool IsIndexOutOfBounds(Vector2Int pos)
+    {
+        return (pos.x < 0 || pos.x >= Width || pos.y < 0 || pos.y >= Height);
+    }
+
+
+    public Vector2 GetWorldPosition(Vector2Int boardPos)
+    {
+        if (_gridToWorldPos.ContainsKeySafe(boardPos))
+        {
+            return _gridToWorldPos[boardPos];
+        }
+        else
+        {
+            return Vector2.negativeInfinity;
+        }
+    }
+
+    public Vector2Int GetPositionOfItem(BoardEntity entity)
+    {
+
+        if (Items == null || Items.Length == 0)
+        {
+            return Vector2Int.down;
+        }
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                BoardEntity item = Items[i, j];
+                if (item != null && item == entity)
+                {
+                    return new Vector2Int(i, j);
+                }
+            }
+        }
+        return Vector2Int.down;
+    }
+
+
+}
+
+
 
 
--- Tap-and-Blast-Dream/Assets/Scripts/Game/Helpers/LevelHelper.cs
+++ Tap-and-Blast-Dream/Assets/Scripts/Game/Helpers/LevelHelper.cs
@@ -7,76 +7,105 @@ using UnityEngine.SceneManagement;
 
 public class LevelHelper : MonoBehaviour
 {
-   private Board _board;
-   private Level _level;
-   private LevelInfo _levelInfo;
+    private Board _board;
+    private Level _level;
+    private LevelInfo _levelInfo;
 
-   private static string _boardPath = "Prefabs/Board/";
-   private Canvas _canvas;
+    private static string _boardPath = "Prefabs/Board/";
+    private Canvas _canvas;
 
-   private  Dictionary<ObstacleTypes, int> _goalRemaining;
+    private Dictionary<ObstacleTypes, int> _goalRemaining;
 
-   public void GenerateLevel(Level level) {
-      _level = level;
-      _goalRemaining  = new Dictionary<ObstacleTypes, int>
+    public void GenerateLevel(Level level)
+    {
+        _level = level;
+        _goalRemaining = new Dictionary<ObstacleTypes, int>
       {
           { ObstacleTypes.Box, 0 },
           { ObstacleTypes.Vase, 0 },
           { ObstacleTypes.Stone, 0 }
       };
-      if (_canvas == null) {
-         _canvas = FindObjectOfType<Canvas>();
-      }
-      if (_board == null && _canvas != null) {
-         _board = Resources.Load<Board>(_boardPath + "Board");
-         _board = Instantiate(_board, _canvas.transform);
-      }
-      if (_board != null) {
-         _board.Set(level.grid_width, level.grid_height, level.grid);
-         //_board.Enable(true);
-      }
-   }
+        if (_canvas == null)
+        {
+            _canvas = FindObjectOfType<Canvas>();
+        }
+        if (_board == null && _canvas != null)
+        {
+            _board = Resources.Load<Board>(_boardPath + "Board");
+            _board = Instantiate(_board, _canvas.transform);
+        }
+        if (_board != null)
+        {
+            _board.Set(level.grid_width, level.grid_height, level.grid);
+            //_board.Enable(true);
+        }
+    }
 
-   public LevelInfo GetLevelInfo() {
-      return new LevelInfo(GetGoal(_board), _level.move_count);
-   }
-   private Dictionary<ObstacleTypes, int> GetGoal(Board board) {
-      List<Obstacle> ObstacleList = board.GetObstacles();
-      for(int i = 0; i < ObstacleList.Count; i++) {
-         ObstacleTypes type = ObstacleList[i].Type;
-         if (type != ObstacleTypes.None) {
-            _goalRemaining[type]++;
-         }
-      }
-      return _goalRemaining;
-   }
+    public LevelInfo GetLevelInfo()
+    {
+        return new LevelInfo(GetGoal(_board), _level.move_count);
+    }
+    private Dictionary<ObstacleTypes, int> GetGoal(Board board)
+    {
+        List<Obstacle> ObstacleList = board.GetObstacles();
+        for (int i = 0; i < ObstacleList.Count; i++)
+        {
+            ObstacleTypes type = ObstacleList[i].Type;
+            if (type != ObstacleTypes.None)
+            {
+                _goalRemaining[type]++;
+            }
+        }
+        return _goalRemaining;
+    }
 
-   public Board GetBoard() {
-      return _board;
-   }
-   public bool IsLevelOver() {
-      return (_level.move_count < 1 || _goalRemaining.Count < 1); 
-   }
+    public Board GetBoard()
+    {
+        return _board;
+    }
+    public bool IsLevelOver()
+    {
+        return (_level.move_count < 1 || _goalRemaining.Count < 1);
+    }
 
-   //TODO
-   public void HandleBlast(List<Blastable> list) {
-      //call event to change move count
-      _board.Enable(false);
-      Vector2Int pos = Vector2Int.zero;
+    //TODO
+    public void HandleBlast(List<Blastable> list)
+    {
+        //call event to change move count
+        _board.Enable(false);
+        Vector2Int pos = Vector2Int.zero;
+        int itemsToAnimate = 0;
+        foreach (Blastable item in list)
+        {
+            if (item.TryGetComponent(out IAnimatable animatable))
+            {
+                pos = _board.GetPositionOfItem(item);
+                animatable.Animate(() =>
+                {
+                    _board.RemoveItem(item);
+                    itemsToAnimate++;
+                    if (itemsToAnimate == list.Count)
+                    {
+                        FillEmptySpaces();
+                    };
+                });
+            }
+            else
+            {
+                _board.RemoveItem(item);
+                itemsToAnimate++;
+                if (itemsToAnimate == list.Count)
+                {
+                    FillEmptySpaces();
+                }
+            }
 
-      foreach(Blastable item in list) {
-         if (item.TryGetComponent(out IAnimatable animatable)) {
-            pos = _board.GetPositionOfItem(item);
-            animatable.Animate(() => { _board.RemoveItem(item);});
-         } else {
-            _board.RemoveItem(item);
-         }
-      }
-   }
-   
-   public void FillEmptySpaces() {
-      _board.ReplaceItemsAfterBlast();
-      //_board.Enable(true);
-   }
+        }
+    }
+
+    public void FillEmptySpaces()
+    {
+        _board.ReplaceItemsAfterBlast();
+    }
 
 }
